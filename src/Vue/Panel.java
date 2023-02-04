0af5c2eecb9f02be5913e687dacdd0a9c3317f44
CorentinEsn <75800644+CorentinEsn@users.noreply.github.com>
2023-02-04 23:25:56 +0100

    Affichage dynamique (change en fonction des choix

--- src/Vue/Panel.java
+++ src/Vue/Panel.java
@@ -13,6 +13,8 @@ public class Panel extends JPanel {
     JLabel cardPicture;
     JLabel descriptifTextArea;
     JLabel scoreTextfield;
+    JLabel choix1;
+    JLabel choix2;
 
     Controlleur controlleur;
 
@@ -25,17 +27,30 @@ public class Panel extends JPanel {
 
         scoreTextfield = new JLabel("Score : 0000");
         scoreTextfield.setSize(100,50);
+        scoreTextfield.setBorder(BorderFactory.createLineBorder(Color.black));
         add(scoreTextfield);
 
-        JLabel cardPicture = new JLabel("Carte");
-        cardPicture.setSize(350,450);
+        cardPicture = new JLabel("Carte");
+        cardPicture.setSize(350,350);
         cardPicture.setIcon(new ImageIcon("\\src\\Pictures\\carte.jpg"));
+        cardPicture.setBorder(BorderFactory.createLineBorder(Color.black));
         cardPicture.setBackground(Color.BLACK);
         add(cardPicture);
 
         descriptifTextArea = new JLabel("Descriptif de la carte");
         descriptifTextArea.setSize(300,100);
+        descriptifTextArea.setBorder(BorderFactory.createLineBorder(Color.black));
         add(descriptifTextArea);
+
+        choix1 = new JLabel("Choix 1");
+        choix1.setSize(200,100);
+        choix1.setBorder(BorderFactory.createLineBorder(Color.black));
+        add(choix1);
+
+        choix2 = new JLabel("Choix 2");
+        choix2.setSize(200,100);
+        choix2.setBorder(BorderFactory.createLineBorder(Color.black));
+        add(choix2);
 
         panelStat = new PanelStat(controlleur);
         add(panelStat);
@@ -68,11 +83,23 @@ public class Panel extends JPanel {
         });
 
     }
- public void maj(){
-     scoreTextfield.setLocation((getWidth()-scoreTextfield.getWidth()),getHeight()-scoreTextfield.getHeight());
-     System.out.println(scoreTextfield.getLocation());
-     descriptifTextArea.setLocation((getWidth()-descriptifTextArea.getWidth())/2,(getHeight()-descriptifTextArea.getHeight())/2);
-     panelStat.setLocation((getWidth()-panelStat.getWidth())/2,0);
+    public void maj(){
+        scoreTextfield.setLocation((getWidth()-scoreTextfield.getWidth()),getHeight()-scoreTextfield.getHeight());
+        System.out.println(scoreTextfield.getLocation());
+        cardPicture.setLocation((getWidth()-cardPicture.getWidth())/2,(getHeight()-cardPicture.getHeight())/2);
+        descriptifTextArea.setLocation((getWidth()-descriptifTextArea.getWidth())/2,cardPicture.getY()+cardPicture.getHeight());
+        choix1.setLocation(cardPicture.getX()-choix1.getWidth(), cardPicture.getY()+(cardPicture.getHeight()-choix1.getHeight())/2);
+        choix2.setLocation(cardPicture.getX()+cardPicture.getWidth(), cardPicture.getY()+(cardPicture.getHeight()-choix2.getHeight())/2);
+        panelStat.setLocation((getWidth()-panelStat.getWidth())/2,0);
 
- }
+    }
+
+    public void choix1(){
+        cardPicture.setLocation(cardPicture.getX() + 40, cardPicture.getY());
+        choix1.setLocation(choix1.getX() + 20, choix1.getY());
+    }
+    public void choix2(){
+        cardPicture.setLocation(cardPicture.getX() - 40, cardPicture.getY());
+        choix2.setLocation(choix2.getX() - 20, choix2.getY());
+    }
 }
